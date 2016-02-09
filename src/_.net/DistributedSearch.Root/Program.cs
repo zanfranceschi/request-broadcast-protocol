592cf1031822939596122b7a82584939d97da2b9
zanfranceschi <zanfranceschi@gmail.com>
2016-02-09 04:51:58 -0200

    topshelf removed

--- src/_.net/DistributedSearch.Root/Program.cs
+++ src/_.net/DistributedSearch.Root/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ZeroMQ;
-using System.Xml.Schema;
 
 namespace DistributedSearch.Root
 {
